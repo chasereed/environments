c9c6711cefd93338f3cb4123abf23407ab4b4a6c
Chase Stolworthy <chasestolworthy@gmail.com>
2023-03-07 21:57:31 -0500

    idk

--- rethinking/install.R
+++ rethinking/install.R
@@ -1,18 +1,15 @@
 
-
 options(repos=c(
   "https://mc-stan.org/r-packages/",
   "https://cloud.r-project.org/"
 ))
 
 install.packages(c(
-  "rstan",
+  "BH",
   "cmdstanr"
 ), dependencies = TRUE)
 
 cmdstanr::install_cmdstan()
-
-IRkernel::installspec(user = FALSE)
 
 install.packages(c("coda", "mvtnorm", "devtools", "loo", "dagitty", "shape"))
 devtools::install_github("rmcelreath/rethinking")
